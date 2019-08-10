7292e994a95b09d07b0040832fee7cc83e9caba4
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-08-11 01:01:37 +0530

    Interfaces Defined

--- InversionOfControlUsingUnity/Program.cs
+++ InversionOfControlUsingUnity/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace InversionOfControlUsingUnity
 {
+    public interface Icar
+        {
+        int Icar();
+        }
+
+    public class Maruti: Icar
+    {
+        private int _topSpeed = 100;
+        public int Icar()
+        {
+            return _topSpeed;
+        }
+    }
+
+    public class Mercedes:Icar
+    {
+        private int _topSpeed = 200;
+        public int Icar()
+        {
+            return _topSpeed;
+        }
+    }
+
+    public class Mclaren : Icar
+    {
+        private int _topSpeed = 300;
+        public int Icar()
+        {
+            return _topSpeed;
+        }
+    }
     class Program
     {
+        
         static void Main(string[] args)
         {
+           
 
         }
     }
